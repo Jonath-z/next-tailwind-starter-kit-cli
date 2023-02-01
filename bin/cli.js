56af33ee3868a1919c95f692496b54d1c36de30c
Jonath-z <jonathanzihindula95@gmail.com>
2023-02-01 16:30:36 +0200

    remove bin folder form gitignore

---
+++ bin/cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const executeCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (err) {
+    console.error(`failed to execute ${command}`, err);
+    return false;
+  }
+
+  return true;
+};
+
+// get the repo command from the command line
+const repoName = process.argv[2];
+
+// clone starter kit command
+const checkoutCommand = `git clone https://github.com/Jonath-z/next-tailwind-starter-kit.git ${
+  repoName || "."
+}`;
+
+// checkout and install dependencies command
+const installCommand = repoName
+  ? `cd ${repoName} && npm install`
+  : "npm install";
+
+// cloning startert kit message
+console.log(
+  `cloning the starter kit with name ${repoName || "of the current directory"}`
+);
+
+// execute the command and return true or false if it's succed or failed
+const checkout = executeCommand(checkoutCommand);
+
+// if the command fails so exit with code -1
+if (!checkout) process.exit(-1);
+
+// install dependencies with the given  repo name message
+console.log(
+  `install dependencies for ${
+    repoName || "the project in the current directory"
+  }`
+);
+
+// run install dependencies command
+const installedDependencies = executeCommand(installCommand);
+
+// if dependencies installation fails so exit the process with code -1
+if (!installedDependencies) process.exit(-1);
+
+// if starter kit installed with success
+console.log(
+  `Congratulation! your are ready. Follow the following command to start`
+);
+
+console.log(`cd ${repoName || "."} && npm install`);
